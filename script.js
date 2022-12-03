b54f44b3440b9d85eb77bf21835c50416516fafd
Yuliia Semykina <semykinayuliia@gmail.com>
2022-12-03 20:36:12 +0200

    delete inappropriate files + almost finished ui part

--- script.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function() {
-//     $('main aside div.theme-toggle').on('click', function () {
-//     $('body').toggleClass('dark-mode')
-//     $('div.theme-toggle span:first-child').toggleClass('active')
-//     $('div.theme-toggle span:last-child').toggleClass('active')
-// })
-// })
